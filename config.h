1e5012ad295d7a0a369977436755fd202d0d7055
syaofox <syaofox@gmail.com>
2022-03-07 13:13:22 +0800

    update

--- config.h
+++ config.h
@@ -3,11 +3,12 @@
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-	BLOCK("sb-mail",    	1800, 17),
-	BLOCK("sb-disk",    	1800, 19),
+	BLOCK("sb-network",    	5, 	  18),
+	BLOCK("sb-nettraf",    	1, 	  10),	
 	BLOCK("sb-memory",  	10,   20),
+	BLOCK("sb-disk",    	1800, 19),
 	BLOCK("sb-volume",  	0,    22),
 	BLOCK("sb-battery", 	5,    23),
-	BLOCK("sb-pacpackages", 0,    23),
+	// BLOCK("sb-pacpackages", 0,    25),
 	BLOCK("sb-date",    	1,    24)
 };
