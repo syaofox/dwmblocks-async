c7bf56e97b16f8d2da77d5ec2e92dc633e35ec3f
syaofox <syaofox@gmail.com>
2022-03-07 11:19:25 +0800

    init

--- config.h
+++ config.h
@@ -3,14 +3,11 @@
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-	BLOCK("sb-mail",    1800, 17),
-	BLOCK("sb-music",   0,    18),
-	BLOCK("sb-disk",    1800, 19),
-	BLOCK("sb-memory",  10,   20),
-	BLOCK("sb-loadavg", 5,    21),
-	BLOCK("sb-mic",     0,    26),
-	BLOCK("sb-record",  0,    27),
-	BLOCK("sb-volume",  0,    22),
-	BLOCK("sb-battery", 5,    23),
-	BLOCK("sb-date",    1,    24)
+	BLOCK("sb-mail",    	1800, 17),
+	BLOCK("sb-disk",    	1800, 19),
+	BLOCK("sb-memory",  	10,   20),
+	BLOCK("sb-volume",  	0,    22),
+	BLOCK("sb-battery", 	5,    23),
+	BLOCK("sb-pacpackages", 0,    23),
+	BLOCK("sb-date",    	1,    24)
 };
