edcf27388f4962e862e3eb659b3ad4145dbdce04
syaofox <syaofox@gmail.com>
2022-03-28 17:04:58 +0800

    sb-smb

--- config.h
+++ config.h
@@ -3,16 +3,17 @@
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-	BLOCK("sb-nettraf",    	1, 	  10),
-	BLOCK("sb-network",    	5, 	  18),
+	BLOCK("sb-nettraf",    	1, 	  8),
+	BLOCK("sb-network",    	5, 	  7),
 	
+	BLOCK("sb-smb",  		0,    9),
 
-	BLOCK("sb-cpu",  		10,   21),
-	BLOCK("sb-memory",  	10,   20),
-	BLOCK("sb-disk",    	1800, 19),
+	BLOCK("sb-cpu",  		10,   6),
+	BLOCK("sb-memory",  	10,   5),
+	BLOCK("sb-disk",    	1800, 4),
 
-	BLOCK("sb-volume",  	0,    22),
-	BLOCK("sb-battery", 	5,    23),
+	BLOCK("sb-volume",  	0,    3),
+	BLOCK("sb-battery", 	5,    2),
 
-	BLOCK("sb-date",    	1,    24)
+	BLOCK("sb-date",    	1,    1)
 };
